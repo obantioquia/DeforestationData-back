6e66bad908a588bc86d33c560c502d55c145b3a6
obantioquia <obantioquia@gmail.com>
2024-12-02 09:52:09 -0500

    Actualización datos reporte back

--- Scripts/IDEAM_data_processing.R
+++ Scripts/IDEAM_data_processing.R
@@ -166,45 +166,45 @@ source("/Users/investigadora/Desktop/OBA_REPORTES_GFW/DeforestationData-back/Scr
 consulta_GFW_data_vec <- Vectorize(consulta_GFW_data)
 
 if(length(countAlertas) %% 2 == 0){
-  grupo <- rep(1:(length(countAlertas)/2), each = 2)
-  total_quincenal <- tapply(countAlertas, grupo, sum)
-  
-  fecha_fin <- fecha.fin
-  fecha_alm <- c()
-  for(i in 1:length(total_quincenal)){
-    fecha_fin <- fecha_fin - 14
-    fecha_alm <- c(fecha_alm, fecha_fin)
-  }
-  
-  fecha_alm <- as.Date(fecha_alm)
-  fecha_inicio <- fecha_alm - 14
-  
-  alertas_GFW_hist <- consulta_GFW_data_vec(fecha_inicio, fecha_alm)
-  
-  data_histG <- read.csv2("/Users/investigadora/Desktop/OBA_REPORTES_GFW/Dashboard-webpage/data_historicov2.csv")
-  
-  if(dim(data_histG)[2] > 3){
-    data_histG <- data_histG[, (dim(data_histG)[2] - 2): dim(data_histG)[2]]
-  }else{
-    data_histG <- data_histG
-  }
-  
-  
-  data_historico <- data.frame(FechaPublicacion = fecha_alm,
-                               AlertasIDEAM = total_quincenal,
-                               AlertasGFW = alertas_GFW_hist)
-  
-  data_historico <- data_historico[order(data_historico$FechaPublicacion),]
-  data_historico$FechaPublicacion <- as.character(data_historico$FechaPublicacion)
-  
-  data_hist_export <- rbind(data_histG, data_historico)
-  
-  write.csv2(data_hist_export, "/Users/investigadora/Desktop/OBA_REPORTES_GFW/Dashboard-webpage/data_historicov2.csv")
-  
-  
-} else {
-  
-  # Trabajar en esto
-  print("Registros impares")
-  
+  countAlertas <- countAlertas
+} else{
+  countAlertas <- countAlertas[1:(length(countAlertas)-1)]
 }
+
+
+grupo <- rep(1:(length(countAlertas)/2), each = 2)
+total_quincenal <- tapply(countAlertas, grupo, sum)
+
+fecha_fin <- fecha.fin
+fecha_alm <- c()
+for(i in 1:length(total_quincenal)){
+  fecha_fin <- fecha_fin - 14
+  fecha_alm <- c(fecha_alm, fecha_fin)
+}
+
+fecha_alm <- as.Date(fecha_alm)
+fecha_inicio <- fecha_alm - 14
+
+alertas_GFW_hist <- consulta_GFW_data_vec(fecha_inicio, fecha_alm)
+
+data_histG <- read.csv2("/Users/investigadora/Desktop/OBA_REPORTES_GFW/Dashboard-webpage/data_historicov2.csv")
+
+if(dim(data_histG)[2] > 3){
+  data_histG <- data_histG[, (dim(data_histG)[2] - 2): dim(data_histG)[2]]
+}else{
+  data_histG <- data_histG
+}
+
+
+data_historico <- data.frame(FechaPublicacion = fecha_alm,
+                             AlertasIDEAM = total_quincenal,
+                             AlertasGFW = alertas_GFW_hist)
+
+data_historico <- data_historico[order(data_historico$FechaPublicacion),]
+data_historico$FechaPublicacion <- as.character(data_historico$FechaPublicacion)
+
+data_hist_export <- rbind(data_histG, data_historico)
+
+write.csv2(data_hist_export, "/Users/investigadora/Desktop/OBA_REPORTES_GFW/Dashboard-webpage/data_historicov2.csv")
+
+
